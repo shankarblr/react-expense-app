05d44266e38e8631e50ef7766b6063420c92b904
Shankar Subramanyam <shankarblr@hotmail.com>
2020-04-16 22:15:48 -0500

    Change to new feature Format currenty and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseItem = ({dispatch,id,description,amount,createdAt}) => (
         <div>
             <Link to={`/edit/${id}`} ><h3>{description}</h3></Link>
-            <p>{amount} - {createdAt} </p>
+            <p>
+                {numeral(amount).format('$0,0.00')} 
+                - 
+                {moment(createdAt).format('MMM Do, YYYY')} 
+            </p>
         </div>
         
 );
